bb2ceb7d5b7d09a0fba6d550246f828623425cb5
sue-ellenn <79308048+sue-ellenn@users.noreply.github.com>
2023-09-08 11:39:35 +0200

    Update ArduinoPong.ino
    hello world

--- ArduinoPong/ArduinoPong.ino
+++ ArduinoPong/ArduinoPong.ino
@@ -4,25 +4,19 @@ The libraries can be added via the library manager: make sure you have verion:
 "Adafruit SSD1306 Wemos Mini OLED" by "Adafruit + mcauser", see below
 */
 
-
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 // #include <SSD1306.h>
-
 
 #define KEYA D3
 #define KEYB D4
 #define OLED_RESET -1  // GPIO1
 
-
 Adafruit_SSD1306 display(OLED_RESET);
-
 
 // screen limitis
 int maxPixelX = 63;
 int maxPixelY = 47;
-
-
 
 
 // coordinates line(paddle)
@@ -32,24 +26,19 @@ int length = 16;
 int buttonA = 0;
 int buttonB = 0;
 
-
 //coordinates ball
 int centerX = 20;
-int centerY= 20;
+int centerY = 20;
 int radius = 3;
 int velocityX = 3;
 int velocityY = 3;
 
-
 // boundaries ball
-int leftBound = centerX - radius;
-int rightBound = centerX + radius;
-int upperBound = centerY - radius;
-int lowerBound = centerY + radius;
-bool inBounds = ((leftBound > 0) && (rightBound < maxPixelX) && (upperBound > 0) && (lowerBound < (maxPixelY - 1)));
-
-
-
+int leftBound = radius;
+int rightBound = maxPixelX - radius;
+int upperBound =  radius;
+int lowerBound = maxPixelY - radius;
+bool inBounds = ((centerX > leftBound) && (centerX < rightBound) && (centerY > upperBound) && (centerY < (lowerBound - 1)));
 
 
 
@@ -58,18 +47,13 @@ void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C address
   delay(1000);
 
-
   pinMode(KEYA, INPUT);
   pinMode(KEYB, INPUT);
- 
 
 
   display.drawCircle(centerX, centerY, radius, WHITE);  // (Xcenter, Ycenter, radius)
   display.fillCircle(centerX, centerY, radius, WHITE);
 }
-
-
-
 
 
 
@@ -81,17 +65,14 @@ void loop() {
   // display.println("NDL");
   // display.setTextSize(1);
 
-
   // display.print("Key A = ");
   // display.println(digitalRead(KEYA));
   // display.print("Key B = ");
   // display.println(digitalRead(KEYB));
 
-
   display.drawFastHLine(x, y, length, WHITE);  // (x, y, length, color)
   buttonA = digitalRead(KEYA);
   buttonB = digitalRead(KEYB);
-
 
   if (buttonA != 1 && x > 0) {
     x = x - 1;
@@ -101,48 +82,36 @@ void loop() {
   display.drawCircle(centerX, centerY, radius, WHITE);  // (Xcenter, Ycenter, radius)
   display.fillCircle(centerX, centerY, radius, WHITE);
 
-
-bool inBounds = ((leftBound > 0) && (rightBound < maxPixelX) && (upperBound > 0) && (lowerBound < (maxPixelY - 1)));
+inBounds = ((centerX > leftBound) && (centerX < rightBound) && (centerY > upperBound) && (centerY < (lowerBound - 1)));
   Serial.print(inBounds);
-
 
   if (inBounds != 1) {
     centerX = centerX + 1;
     centerY = centerY + 1;
   }
- 
 
 
   /*
 // '1' should become a velocity variable
-
 
 initially
 centerX = centerX + 1
 centerY = centerY + 1
 
 
-
-
 if (hit wallRight || hit wallLeft){
   velocityX = - velocityX
   }
-
 
 else if (hit top || hit bottom){
   velocityY = - velocityY
   }
 
-
   centerX update
   centerY update
 
 
-
-
 */
-
-
 
 
   display.display();  // required to refresh the screen contents
@@ -150,16 +119,12 @@ else if (hit top || hit bottom){
 }
 
 
-
-
 /*
 // HELLO WORLD CODE
-
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);  // initialize pin as output
 }
-
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // switch LED on (HIGH voltage)
@@ -168,6 +133,3 @@ void loop() {
   delay(1000);                      // wait a second
 }
 */
-
-
-
